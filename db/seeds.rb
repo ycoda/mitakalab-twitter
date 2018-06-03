0e44263a44038985587f114276b44069aa5e5880
ycoda <tankaku@gmail.com>
2018-06-03 13:17:24 +0900

    railsの基本を終えた

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user = User.new
+@user.name = 'Ryo Suzuki'
+@user.username = 'ryooopan'
+@user.location = 'Kanagawa, Japan'
+@user.about = 'Hello, I am Ryo. I am from database!'
+@user.save
+
+@user = User.new
+@user.name = 'Shohei Aoki'
+@user.username = 'moyahima'
+@user.location = 'Tottori, Japan'
+@user.about = 'Nice to meet you. I am from database!'
+@user.save
