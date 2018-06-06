b8bb617ea7b6623c8f55ef36ec7be5c0d5164077
ycoda <tankaku@gmail.com>
2018-06-06 19:57:07 +0900

    ルーティングをresoucesで書いた

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/new'
-  post 'tweets' => 'tweets#create'
+  devise_for :users
+  resources :tweets, only: [:index, :new, :create]
 
-  get 'users/index'
-  get 'users/show/:username' => 'users#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index' => 'users#index'
+  get 'users/show/:id' => 'users#show'
+  root to: 'tops#index'
 end
