7afabd2db1bb72bba535c1112965d6b3d84c7043
ycoda <tankaku@gmail.com>
2018-06-18 19:31:07 +0900

    ユーザー新規登録時に画像アイコンを追加できるようにした

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,11 +4,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   def create
-    @user = User.new(
-      name: params[:user][:name],
-      email: params[:user][:email],
-      password: params[:user][:password]
-    )
+    @user = User.new(user_params)
     if @user.save
       sign_in_and_redirect @user, :event => :authentication
     else
@@ -21,5 +17,9 @@ class Users::RegistrationsController < Devise::RegistrationsController
       hash[:uid] = User.create_unique_string
       super
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :avatar)
   end
 end
