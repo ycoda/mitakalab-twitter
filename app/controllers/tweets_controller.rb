0c3c237fb7b4a2d72fb1fe7489afb84d0a54eb43
ycoda <tankaku@gmail.com>
2018-06-29 21:55:30 +0900

    destroyアクションを作った

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,11 +11,22 @@ class TweetsController < ApplicationController
   end
 
   def create
-    @tweet = Tweet.new(title: params[:tweet][:title], content: params[:tweet][:content], user_id: current_user.id)
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
     if @tweet.save
       redirect_to tweets_path
     else
       render 'new'
     end
   end
+
+  def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+  end
+
+  private
+    def tweet_params
+      params.require(:tweet).permit(:title, :content)
+    end
 end
