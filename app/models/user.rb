14a544a0b1c60d1173c3564654eb69dddf9b562b
ycoda <tankaku@gmail.com>
2018-06-29 21:59:13 +0900

    バリデーションを設定した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ApplicationRecord
+  has_many :tweets
   has_one_attached :avatar
+
+  validates :name_or_username, presence: true
+  validates :email, presence: true
+  validates :name, :email, uniqueness: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,14 +13,14 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:twitter]
 
   def self.find_for_twitter_oauth(auth)
-    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    user = User.find_by(:provider => auth.provider, :uid => auth.uid)
     unless user
-    user = User.create( username: auth.info.name,
-                        provider: auth.provider,
-                        uid:      auth.uid,
-                        email:    User.create_unique_email,
-                        password: Devise.friendly_token[0,20]
-                       )
+      user = User.create( username: auth.info.name,
+                          provider: auth.provider,
+                          uid:      auth.uid,
+                          email:    User.create_unique_email,
+                          password: Devise.friendly_token[0,20]
+                         )
     end
     user
   end
@@ -29,4 +34,9 @@ class User < ApplicationRecord
   def self.create_unique_email
    User.create_unique_string + "@example.com"
   end
+
+  private
+    def name_or_username
+      name.presence || username.presence
+    end
 end
